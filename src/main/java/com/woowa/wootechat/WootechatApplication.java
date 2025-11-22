2d2048856465baf648ea931bc13a3032f6e78ca7
yu-teacher <yusm1231@gmail.com>
2025-11-23 07:04:42 +0900

    최초 커밋

---
+++ src/main/java/com/woowa/wootechat/WootechatApplication.java
@@ -0,0 +1,13 @@
+package com.woowa.wootechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WootechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WootechatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woowa/wootechat/WootechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowa.wootechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WootechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
